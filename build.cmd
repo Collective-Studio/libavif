adb3da1d4521128d7d8109fd3cfe77fb73a261e1
hoangvietfreelancer <29736874+hoangvietfreelancer@users.noreply.github.com>
2021-01-17 22:00:33 +0700

    Create build.cmd

---
+++ build.cmd
@@ -0,0 +1,14 @@
+git clone --depth 1 https://github.com/AOMediaCodec/libavif.git
+
+# cmake and ninja must be in your PATH.
+cd libavif/ext/
+./aom.cmd
+./libjpeg.cmd
+./libyuv.cmd
+./zlibpng.cmd
+cd ..
+mkdir build
+cd build
+
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DAVIF_CODEC_AOM=ON -DAVIF_LOCAL_AOM=ON -DAVIF_BUILD_APPS=ON -DAVIF_LOCAL_JPEG=1 -DAVIF_LOCAL_ZLIBPNG=1 -DAVIF_LOCAL_LIBYUV=1 ..
+ninja
